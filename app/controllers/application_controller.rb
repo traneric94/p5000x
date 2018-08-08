5a0652c72176da1ab2cf07d97cf1da057da2b7c6
traneric94 <traneric94@gmail.com>
2018-08-07 17:48:14 -0700

    Heroku Deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  private
+
+  def current_user
+  end
+
+  def logged_in?
+  end
+
+  def login(user)
+  end
 end
