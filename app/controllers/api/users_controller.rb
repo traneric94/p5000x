02843cc54cf18d0f513811fae74ec93a98edc92d
traneric94 <traneric94@gmail.com>
2018-08-07 20:21:19 -0700

    Finish User Auth Backend

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,10 +1,17 @@
 class Api::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
+
+    if @user.save
+      login(@user)
+      render "api/users/show"
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
   end
 
   private
-  
+
   def user_params
     params.require(:user).permit(
       :username,
@@ -13,4 +20,5 @@ class Api::UsersController < ApplicationController
       :profile_pic_url
     )
   end
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,29 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
   end
 
   def logged_in?
+    !!current_user
   end
 
   def login(user)
+    user.reset_session_token!
+    session[:session_token] = user.session_token
+    @current_user = user
   end
+
+  def require_login
+    unless current_user
+      render json: { base: ['invalid credentials'] }, status: 401
+    end
+  end
+
+  def logout
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    @current_user = nil
+  end
+
 end
