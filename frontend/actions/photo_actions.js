aea9f4d3209b5eb5f7d23a25511ad1e27b3b849e
traneric94 <traneric94@gmail.com>
2018-08-10 15:53:40 -0700

    heroku AWS deploy

--- frontend/actions/photo_actions.js
+++ frontend/actions/photo_actions.js
@@ -2,3 +2,41 @@ import * as PhotoAPIUtil form '../util/user_util';
 
 export const RECEIVE_PHOTOS = "RECEIVE_PHOTOS";
 export const RECEIVE_PHOTO = "RECEIVE_PHOTO";
+export const RECEIVE_PHOTO_ERRORS = "RECEIVE_PHOTO_ERRORS";
+
+const receivePhotos = photos => ({
+  type: RECEIVE_PHOTOS,
+  photos
+});
+
+const receivePhoto = photos => ({
+  type: RECEIVE_PHOTO,
+  photo
+});
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_PHOTO_ERRORS,
+  errors
+});
+
+export const photoIndex = () => dispatch => (
+  PhotoAPIUtil.getPhotos().then(
+    photos => (
+      dispatch(receivePhotos())
+    ),
+    err => (
+      dispatch(receiveErrors(err.response.JSON))
+    )
+  )
+);
+
+export const photoShow = (id) => dispatch => (
+  PhotoAPIUtil.getPhoto(id).then(
+    photo => (
+      dispatch(receivePhoto())
+    ),
+    err => (
+      dispatch(receiveErrors(err.response.JSON))
+    )
+  )
+);
